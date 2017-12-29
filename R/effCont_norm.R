91115d7c76e9d16a924d82f7f29f9289fed33bcc
julian-urbano <urbano.julian@gmail.com>
2017-12-30 00:46:10 +0100

    Added truncated normal distribution.

---
+++ R/effCont_norm.R
@@ -0,0 +1,52 @@
+#' Continuous Effectiveness as Truncated Normal Distribution.
+#'
+#' Fits a Normal distribution, truncated between 0 and 1, to the given sample of scores.
+#'
+#' @param x a sample of effectiveness scores between 0 and 1.
+#' @return an object of class \code{effCont_norm}, which inherits from \code{effCont}.
+#'
+#' @examples
+#' @todo
+#'
+#' @seealso \code{\link{deff}}, \code{\link{peff}}, \code{\link{qeff}} and \code{\link{reff}}.
+#' @author Julián Urbano
+#' @export
+effCont_norm<- function(x) {
+  # estimate parameters and truncated functions
+  mu0 <- mean(x)
+  sigma0 <- sd(x)
+
+  fit <- MASS::fitdistr(x, densfun = function(xx, mu, sigma) {
+    truncnorm::dtruncnorm(xx, a = 0, b = 1, mean = mu, sd = sigma)
+  }, start = list(mu = mu0, sigma = sigma0),
+  lower = list(mu = -Inf, sigma = .02),
+  upper = list(mu = Inf, sigma = Inf))
+  mu <- as.numeric(fit$estimate[1])
+  sigma <- as.numeric(fit$estimate[2])
+
+  E <- truncnorm::etruncnorm(a = 0, b = 1, mean = mu, sd = sigma) # expected value
+  Var <- truncnorm::vtruncnorm(a = 0, b = 1, mean = mu, sd = sigma) # variance
+
+  # prepare eff object and return
+  e <- effCont_new(E, Var, 2, x)
+  e$model <- list(mean = mu, sd = sigma)
+  class(e) <- c("effCont_norm", class(e))
+  e
+}
+
+#' @export
+deff.effCont_norm <- function(x, eff) {
+  truncnorm::dtruncnorm(x, a = 0, b = 1, mean = eff$model$mean, sd = eff$model$sd)
+}
+#' @export
+peff.effCont_norm <- function(q, eff) {
+  truncnorm::ptruncnorm(q, a = 0, b = 1, mean = eff$model$mean, sd = eff$model$sd)
+}
+#' @export
+qeff.effCont_norm <- function(p, eff) {
+  truncnorm::qtruncnorm(p, a = 0, b = 1, mean = eff$model$mean, sd = eff$model$sd)
+}
+#' @export
+reff.effCont_norm <- function(n, eff) {
+  truncnorm::rtruncnorm(n, a = 0, b = 1, mean = eff$model$mean, sd = eff$model$sd)
+}
