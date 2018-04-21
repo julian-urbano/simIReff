24a1cbab7d36f44863d831951099764985763693
julian-urbano <urbano.julian@gmail.com>
2018-04-21 20:48:23 +0200

    Reimplement effXFit with NA rather than NULL.

--- R/effFit.R
+++ R/effFit.R
@@ -1,21 +1,21 @@
 #' @export
 effContFit <- function(x, silent = TRUE) {
-  effs <- vector("list", 4)
+  effs <- as.list(rep(NA, 4))
 
   try(effs[[1]] <- effCont_norm(x), silent = silent)
   try(effs[[2]] <- effCont_beta(x), silent = silent)
   try(effs[[3]] <- effCont_nks(x), silent = silent)
   try(effs[[4]] <- effCont_bks(x), silent = silent)
 
-  effs <- effs[!sapply(effs, is.null)]
+  effs <- effs[!is.na(effs)]
   if(length(effs) == 0)
-    stop("Unable to fit a distribution!")
+    stop("Unable to fit a distribution")
   effs
 }
 
 #' @export
 effDiscFit <- function(x, support, silent = TRUE) {
-  effs <- vector("list", 5)
+  effs <- as.list(rep(NA, 5))
 
   try(effs[[1]] <- effDisc_bbinom(x, support), silent = silent)
   try(effs[[2]] <- effDisc_np(x, support), silent = silent)
@@ -23,8 +23,8 @@ effDiscFit <- function(x, support, silent = TRUE) {
   try(effs[[4]] <- effDisc_np(x, support, mult = 5), silent = silent)
   try(effs[[5]] <- effDisc_np(x, support, mult = 10), silent = silent)
 
-  effs <- effs[!sapply(effs, is.null)]
+  effs <- effs[!is.na(effs)]
   if(length(effs) == 0)
-    stop("Unable to fit a distribution!")
+    stop("Unable to fit a distribution")
   effs
 }
