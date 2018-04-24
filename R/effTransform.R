1770255160f331b3b0200bda8d7b2c0719e51379
julian-urbano <urbano.julian@gmail.com>
2018-04-24 03:15:38 +0200

    Correct optim tolerance in effTransform.

--- R/effTransform.R
+++ R/effTransform.R
@@ -1,35 +1,36 @@
 #' @export
-effTransform <- function(eff, mean = base::mean(eff$data), abs.tol = 1e-6) {
-  # For some reason, optim fails when the starting values yield a valid solution, so try it first
-  # tdist <- effdistTransform(dist,1,1)
-  #if(abs(tdist$mu-mu)<abs.tol)
-  # return(tdist)
+effTransform <- function(eff, mean, abs.tol = 1e-5) {
+  m <- base::mean(eff$data)
+  if(!missing(mean))
+    m <- mean
 
   opt <- optim(par = c(1, 1),
                fn = function(par) {
                  teff <- effTransform_inner(eff, par[1], par[2])
-                 return((teff$mean - mean)^2)
+                 return((teff$mean - m)^2)
                },
                method = "L-BFGS-B",
                lower = c(1, 1),
-               control = list(factr = abs.tol / .Machine$double.eps))
+               control = list(factr = abs.tol^2 / .Machine$double.eps))
 
   if(opt$convergence != 0)
     stop("No successful transformation")
-  return(effTransform_inner(eff, opt$par[1], opt$par[2]))
+  effTransform_inner(eff, opt$par[1], opt$par[2])
 }
 
 #' @export
-effTransformAll <- function(effs, mean = base::mean(effs[[1]]$data), silent = TRUE, ...) {
-  teffs <- vector("list", length(effs))
+effTransformAll <- function(effs, means, silent = TRUE, ...) {
+  if(missing(means))
+    means <- sapply(effs, function(e) mean(e$data))
+
+  means <- means + rep(0, length(effs))
+  teffs <- as.list(rep(NA, length(effs)))
 
   for(i in seq_along(effs)) {
-    e <- try(effTransform(effs[[i]], mean = mean, ...), silent = silent)
+    e <- try(effTransform(effs[[i]], mean = means[i], ...), silent = silent)
 
     if(class(e) != "try-error")
       teffs[[i]] <- e
-    else
-      teffs[[i]] <- NULL
   }
 
   teffs
@@ -39,7 +40,7 @@ effTransform_inner <- function(eff, par1, par2) {
   if(inherits(eff, "eff.cont")) { # continuous
 
     dfun <- function(x) {
-      x <- cap(x)
+      #x <- cap(x)
       dbeta(peff(x, eff), par1, par2) * deff(x, eff)
     }
     pfun <- function(q) {
@@ -62,7 +63,7 @@ effTransform_inner <- function(eff, par1, par2) {
   } else { # discrete
     # compute the new cdf values, and from there the full object
     p <- pbeta(peff(eff$support, eff), par1, par2)
-    p <- cummax(p) # ensure it's monotonically
+    p <- cummax(p) # ensure it's monotonically increasing
 
     teff <- effDisc_new(p, eff$support, eff$df, eff$data)
     teff$model <- list(type = paste0("t(", eff$model$type, ")"),
